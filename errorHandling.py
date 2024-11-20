c06e01b299d3db9041fe6c17f211bb8ba52b1fe1
vinnietito <vincentnjuguna69@gmail.com>
2024-11-20 11:29:28 +0300

    Commits

--- errorHandling.py
+++ errorHandling.py
@@ -4,7 +4,12 @@ b = "h"
 #print(a/b)
 try:
     print(a/b)
+    k = int(input("ENter a number: "))
 except ZeroDivisionError:
     print("You can't divide this with zero")
+except ValueError:
+    print("We are a wrong value")
 except Exception:
     print("This is a very Serious Error")
+finally:
+    print("Thank you for banking with us")
