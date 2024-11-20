07bb60038853dbd14ac67a680afd5210ca30a4fb
vinnietito <vincentnjuguna69@gmail.com>
2024-11-20 10:38:08 +0300

    Commits

--- openFile.py
+++ openFile.py
@@ -2,7 +2,14 @@
 
 # print(f1)
 
-f1 = open("myDataFile.txt", "r")
-content = f1.read()
-print(content)
-f1.close()
+
+# f1 = open("myDataFile.txt", "r")
+# content = f1.read()
+# print(content)
+# f1.close()
+
+
+# f1 = open("myDataFile.txt", "r")
+# for line in f1:
+#     print(line.strip())  # `.strip()` removes leading/trailing whitespaces including `\n`
+# f1.close()
